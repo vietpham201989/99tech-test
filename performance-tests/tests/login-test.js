a77bd0a3bf10efa25d83c4426d5ee28ac544f0a1
vietpham201989 <vietpham204@gmail.com>
2026-08-17 23:30:00 +0700

    feat: add k6 summary report generation and update report paths

--- performance-tests/tests/login-test.js
+++ performance-tests/tests/login-test.js
@@ -3,6 +3,8 @@
 import http from "k6/http"
 import { check, sleep } from "k6"
 import encoding from "k6/encoding"
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js"
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.1.0/index.js"
 const accData = JSON.parse(open("../../data/account.json"))
 const configData = JSON.parse(open("../../data/api/config.json"))
 const env = __ENV.ENV || 'stg';
@@ -46,4 +48,11 @@ export default function () {
     "response time < 500ms": (r) => r.timings.duration < 500,
   })
   sleep(1)
+}
+
+export function handleSummary(data) {
+  return {
+    "reports/k6-summary.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  }
 }
